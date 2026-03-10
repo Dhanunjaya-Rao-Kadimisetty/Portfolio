971592664957e7c7f8614510c7694ae5da56af35
Dhanunjaya Rao Kadimisetty <saidhanunjaya19@gmail.com>
2026-03-10 07:37:08 +0530

    Support GitHub Pages and Vercel builds

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/Portfolio/",
-  plugins: [react()],
-  server:{
-    port:5173
-  }
-})
+export default defineConfig(() => {
+  const isGitHubPagesBuild = process.env.DEPLOY_TARGET === "github-pages";
+
+  return {
+    base: isGitHubPagesBuild ? "/Portfolio/" : "/",
+    plugins: [react()],
+    server: {
+      port: 5173,
+    },
+  };
+});
